9f2c771711ea73b35950bd05560fee6d32362941
Paul Riseborough <gncsolns@gmail.com>
2019-03-05 15:00:27 +1100

    AC_Loiter:  Remove accel max param sanity check
    The memthod implemented does nto work for tilt rotors for  tilt range >= 90 degrees

--- libraries/AC_WPNav/AC_Loiter.cpp
+++ libraries/AC_WPNav/AC_Loiter.cpp
@@ -211,7 +211,6 @@ void AC_Loiter::update()
 void AC_Loiter::sanity_check_params()
 {
     _speed_cms = MAX(_speed_cms, LOITER_SPEED_MIN);
-    _accel_cmss = MIN(_accel_cmss, GRAVITY_MSS * 100.0f * tanf(ToRad(_attitude_control.lean_angle_max() * 0.01f)));
 }
 
 /// calc_desired_velocity - updates desired velocity (i.e. feed forward) with pilot requested acceleration and fake wind resistance
