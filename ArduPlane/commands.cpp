7926920d0181ebea6251b5f522aed849d11900cb
Paul Riseborough <priseborough@sypaq.com.au>
2019-04-26 18:00:52 +1000

    ArduPlane: Demote waypoint reset message
    Reduces verbosity during standard missions

--- ArduPlane/commands.cpp
+++ ArduPlane/commands.cpp
@@ -51,7 +51,7 @@ void Plane::set_next_WP(const struct Location &loc)
     // location as the previous waypoint, to prevent immediately
     // considering the waypoint complete
     if (location_passed_point(current_loc, prev_WP_loc, next_WP_loc)) {
-        gcs().send_text(MAV_SEVERITY_NOTICE, "Resetting previous waypoint");
+        gcs().send_text(MAV_SEVERITY_INFO, "Resetting previous waypoint");
         prev_WP_loc = current_loc;
     }
 
