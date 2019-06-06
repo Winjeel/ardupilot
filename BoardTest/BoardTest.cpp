5dd6e0225fa66f74764a934e4e0476b6dafd9a39
Nick Exton <NExton@sypaq.com.au>
2019-06-06 16:25:24 +1000

    AP-23: WIP checkpoint
    (cherry picked from commit a7ac0343c011932f43eafb326adae2f330e03ace)

--- BoardTest/BoardTest.cpp
+++ BoardTest/BoardTest.cpp
@@ -3,7 +3,14 @@
  */
 
 #include <AP_HAL/AP_HAL.h>
+
+#include <AP_BoardConfig/AP_BoardConfig.h>
+#include <AP_Common/AP_FWVersion.h>
+
 #include <AP_Baro/AP_Baro.h>
+#include <AP_Compass/AP_Compass.h>
+#include <AP_InertialSensor/AP_InertialSensor.h>
+#include <AP_SerialManager/AP_SerialManager.h>
 
 #include <ctype.h>
 
@@ -11,6 +18,14 @@ void setup();
 void loop();
 
 const AP_HAL::HAL& hal = AP_HAL::get_HAL();
+
+static AP_BoardConfig boardConfig;
+
+// sensor declaration
+static AP_Baro baro;
+static Compass compass;
+static AP_InertialSensor ins;
+static AP_SerialManager serial_manager;
 
 
 typedef bool (*TestFn)(void);
@@ -67,7 +82,6 @@ static bool _executeTest(Test const * const test) {
 
 
 static bool _testBaro(void) {
-    const AP_Baro &baro = AP::baro();
     return baro.healthy();
 }
 
@@ -116,8 +130,65 @@ static bool _runAll(void) {
 }
 
 
-void setup(void)
-{
+static void _driverInit(void) {
+    // initialise serial port
+    serial_manager.init_console();
+
+    hal.console->printf("\n\nInit %s"
+                        "\n\nFree RAM: %u\n",
+                        AP::fwversion().fw_string,
+                        (unsigned)hal.util->available_memory());
+
+// TODO: move into defines.h?
+#define MASK_LOG_IMU_RAW (1UL<<19)
+    ins.set_log_raw_bit(MASK_LOG_IMU_RAW);
+
+    // initialise serial ports
+    serial_manager.init();
+    // gcs().chan(0).setup_uart(serial_manager, AP_SerialManager::SerialProtocol_MAVLink, 0);
+
+
+    // Register mavlink_delay_cb, which will run anytime you have
+    // more than 5ms remaining in your call to hal.scheduler->delay
+    // hal.scheduler->register_delay_callback(mavlink_delay_cb_static, 5);
+
+    // setup any board specific drivers
+    boardConfig.init();
+
+    // init baro
+    baro.init();
+
+    // initialise rangefinder
+    // rangefinder.init();
+
+    // initialise battery monitoring
+    // battery.init();
+
+    // rpm_sensor.init();
+
+    // setup telem slots with serial ports
+    // gcs().setup_uarts(serial_manager);
+
+    // initialise airspeed sensor
+    // airspeed.init();
+
+    bool compass_ok = compass.init() && compass.read();
+    if (!compass_ok) {
+        hal.console->printf("Compass initialisation failed!\n");
+    } else {
+        // ahrs.set_compass(&compass);
+    }
+
+    // GPS Initialization
+    // #define MASK_LOG_GPS                    (1<<2)
+    // gps.set_log_gps_bit(MASK_LOG_GPS);
+    // gps.init(serial_manager);
+
+    // disable safety if requested
+    boardConfig.init_safety();
+}
+
+static void _consoleInit(void) {
     while (!hal.console->is_initialized()) {
         hal.scheduler->delay(100);
     }
@@ -131,6 +202,13 @@ void setup(void)
     _printHeader();
     _printInstructions();
     hal.console->printf("\nEnter command: ");
+}
+
+
+void setup(void)
+{
+    _driverInit();
+    _consoleInit();
 }
 
 
---
+++ BoardTest/version.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef FORCE_VERSION_H_INCLUDE
+#error version.h should never be included directly. You probably want to include AP_Common/AP_FWVersion.h
+#endif
+
+#include "ap_version.h"
+
+#define THISFIRMWARE "BoardTest V0.0.1-dev"
+
+// the following line is parsed by the autotest scripts
+#define FIRMWARE_VERSION 0,0,1,FIRMWARE_VERSION_TYPE_DEV
+
+#define FW_MAJOR 0
+#define FW_MINOR 0
+#define FW_PATCH 1
+#define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
