670b3823a1204b05c0b246fe7a33156fe438d026
Paul Riseborough <priseborough@sypaq.com.au>
2019-08-11 20:00:42 +1000

    AP_NavEKF3: Remove redundant code left over from previous changes

--- libraries/AP_NavEKF3/AP_NavEKF3_AirDataFusion.cpp
+++ libraries/AP_NavEKF3/AP_NavEKF3_AirDataFusion.cpp
@@ -135,7 +135,7 @@ void NavEKF3_core::FuseAirspeed()
         varInnovVtas = 1.0f/SK_TAS[0];
 
         // calculate measurement innovation
-        if ((externalEasDefault >= 5.0f) && (externalEasDefault >= 0.0f)) {
+        if (externalEasDefault >= 5.0f) {
             innovVtas = VtasPred - externalEasDefault;
         } else {
             innovVtas = VtasPred - tasDataDelayed.tas;
