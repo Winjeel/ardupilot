85481a646c1afa5b299e284cb75b0aaed627687b
Paul Riseborough <priseborough@sypaq.com.au>
2019-09-20 10:21:20 +1000

    Revert "ArduPlane: Add function to check for minimum viable mission plan"
    This reverts commit 942be3477692baa531a8682f7decf91d6f7710ff.

--- ArduPlane/Plane.h
+++ ArduPlane/Plane.h
@@ -985,7 +985,6 @@ private:
     int8_t throttle_percentage(void);
     void change_arm_state(void);
     bool disarm_motors(void);
-    bool check_mission(void);
     bool arm_motors(AP_Arming::Method method, bool do_arming_checks=true);
     bool auto_takeoff_check(void);
     void takeoff_calc_roll(void);
