70be856cf2983afe3c884845d813c51309825625
Paul Riseborough <priseborough@sypaq.com.au>
2019-10-11 13:25:40 +1100

    ArduPlane: Use function to check if landing sector is defined

--- ArduPlane/system.cpp
+++ ArduPlane/system.cpp
@@ -565,7 +565,7 @@ bool Plane::create_into_wind_landing_sequence()
     // Get desired approach heading and constrain if necessary
     // During landing loiter_xtrack is used to indicate if heading constraint data is available
     // The desired approach heading and tolerance are held in the p1 parameter
-    if (land_point_cmd.content.location.loiter_xtrack > 0) {
+    if (plane.mission.isAngleSectorLanding(land_point_cmd)) {
         uint16_t approach_hdg_tol_deg = (land_point_cmd.p1 >> 9) * 2; // yaw angle tolerance is held in high 7 bits of p1
         if (approach_hdg_tol_deg < 180) {
             float approach_hdg_dem_deg = land_point_cmd.p1 & 0x01FF; // yaw angle demand is held in low 9 bits of p1
